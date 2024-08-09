7a5446022ab968c8bf709bcfed999dd0e95af3e9
weisang <1428206861@qq.com>
2024-08-09 19:52:48 +0800

    ok

---
+++ mainwindow.h
@@ -0,0 +1,40 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets>
+
+namespace Ui {
+	class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+	Q_OBJECT
+
+public:
+	explicit MainWindow(QWidget* parent = 0);
+	~MainWindow();
+
+protected:
+	void resizeEvent(QResizeEvent* ev)override;
+	void mousePressEvent(QMouseEvent* ev)override;
+	void mouseReleaseEvent(QMouseEvent* event)override;
+	void mouseMoveEvent(QMouseEvent* event)override;
+	void showEvent(QShowEvent* event)override;
+
+private:
+	int countFlag(QPoint p, int row);
+	void setCursorType(int flag);
+	int countRow(QPoint p);
+
+private:
+	Ui::MainWindow* ui;
+
+	int m_curPos;
+	QPoint pLast;
+
+	bool m_bPressed;
+};
+
+#endif // MAINWINDOW_H
